8a3b5cd92fa3ce45f8c14732a65e567161bd2d85
carterrows <carter.rows@gmail.com>
2025-12-10 22:37:30 -0500

    added browser tab icon - 32x32 favicon

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import subprocess
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
